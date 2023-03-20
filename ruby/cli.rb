db0a33602ed680c2d393be30f533012366aba5bd
alexey <azheltkovskiy@hubspot.com>
2023-03-20 13:14:47 +0300

    update ruby sample

--- ruby/cli.rb
+++ ruby/cli.rb
@@ -1,21 +1,34 @@
 require_relative 'config'
 
 class Cli
-  def run
-    Thread.abort_on_exception = true
-    Thread.report_on_exception = true
-    1000.times do
-      Thread.new { call_api }
-      p :*
-    end
-  end
-
-  private
-
   def call_api
-    client = Hubspot::Client.new(access_token: access_token)
-    api = client.crm.contacts.basic_api.get_page
+    api_client = Hubspot::Client.new(access_token: access_token)
+    retries = 0
+    max_retries = 3
+    while retries < max_retries
+      begin
+        puts "Start requests: #{retries + 1}"
+        api_client.crm.contacts.basic_api.get_page
+        break  # Exit the loop if the API call succeeds
+      rescue Hubspot::Crm::Contacts::ApiError => e
+        if e.code == 429
+          puts 'Rate limit exceeded, retrying in 5 seconds...'
+          sleep(5)
+          retries += 1
+        else
+          raise  # Reraise the exception if it's not a rate limit error
+        end
+      end
+    end
   end
 end
 
-Cli.new.run
+
+threads = []
+200.times do
+  threads << Thread.new do
+    Cli.new.call_api
+  end
+end
+
+threads.each(&:join)
