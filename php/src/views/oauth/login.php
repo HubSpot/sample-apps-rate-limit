bb6df8768391d84f0d2d5de917e340ef3e0b0bd9
ksvirkou-hubspot <ksvirkou@hubspot.com>
2023-11-14 16:11:14 +0300

    Php: Update copy script

--- php/src/views/oauth/login.php
+++ php/src/views/oauth/login.php
@@ -6,7 +6,7 @@ include __DIR__.'/../_partials/header.php';
 <div class="container text-center">
     <h3>In order to continue please update the redirect URL on Auth settings page  of your app</h3>
     <h4>Redirect URL</h4>
-    <pre id="redirectURL"><?php echo OAuth2Helper::getRedirectUri(); ?></pre>
+    <pre id="copyText"><?php echo OAuth2Helper::getRedirectUri(); ?></pre>
     <button id="copyBtn" class="button-primary">Copy</button>
     <h3>After that authorize via OAuth</h3>
     <div class="authorize-button">
@@ -21,5 +21,5 @@ $authUrl = HubSpot\Utils\OAuth2::getAuthUrl(
     ['Scopes']
 );
 </pre>
-<script type="text/javascript" src="/js/login.js?<?php echo filemtime('./js/login.js'); ?>"></script>
+<script type="text/javascript" src="/js/copy.js?<?php echo filemtime('./js/copy.js'); ?>"></script>
 <?php include __DIR__.'/../_partials/footer.php'; ?>
